89b7614bf4eb1404e4278d1ceef3db38adc584b9
mubtesh <mubarekteshome@gmail.com>
2024-10-03 17:24:51 +0300

    ok

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,19 +4,25 @@ const Navbar = () => {
   return (
     <div className="flex justify-end md:justify-between p-4  items-center ">
       <div className="hidden md:flex justify-start items-center gap-2 text-sm rounded-full ring-[1px] ring-gray py-1 px-2">
+        <div className="rounded-full">
         <Image src="/favicon.ico" alt="" width={20} height={20} />
+        </div>
+        
         <input type="text" placeholder="Search..." className="p-1 bg-transparent  outline-none" />
       </div>
 
       <div className="flex gap-2 items-center justify-center rounded-full bg-gray-300 py-1 px-2 relative">
         <div className="flex items-center rounded-full justify-center">
-        <Image
-          src="/favicon.ico"
+          <div className="rounded-full">
+          <Image
+          src="/avatar.png"
           alt=""
           width={20}
           height={20}
           
         />
+          </div>
+        
         <div className="flex items-center  justify-center absolute -top-3 -right-3 w-5 h-5 bg-purple-500 rounded-full text-sm text-white">1</div>
         </div>
         
